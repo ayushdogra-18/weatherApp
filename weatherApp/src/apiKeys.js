54f7df5453679a97f71a5904d5d000ae3f5faf14
ayushdogra-18 <147146430+ayushdogra-18@users.noreply.github.com>
2025-01-28 11:34:44 +0530

    ff

---
+++ weatherApp/src/apiKeys.js
@@ -0,0 +1,7 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "a837707c7b583b0e9e09523b3a3e21d9",
+  base: "https://api.openweathermap.org/data/2.5/",
+ 
+
+};
